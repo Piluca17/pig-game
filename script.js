fed80fac920a5101acfd3da0ccddd603d6b05343
Piluca17 <b.piluca92@gmail.com>
2024-01-26 14:09:11 +0100

    Implementamos la función mostrar dado

--- script.js
+++ script.js
@@ -3,37 +3,45 @@
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
-//const btnHold = document.getElementsByClassName('.btn--hold')
 const diceEl = document.querySelector('.dice')
-const scoreField = document.querySelector('.score')
+const score0El = document.querySelector('#score--0')
+const score1El = document.querySelector('#score--1')
+const current0El = document.querySelector('#current--0')
+const current1El = document.querySelector('#current--1')
 
-console.log (btnNew, btnRoll, btnHold)
+//const btnHold = document.getElementsByClassName('.btn--hold')
 
-//Configurar la aplicación (constantes y variables)
-const INIT_SCORE = 0
+//llamar a función init que haga lo siguiente:
+// el dado por defecto que no se vea
+// score1 y score2 a 0
+// currentScore1 y currentScore2 a 0
 
-//variables de la aplicación
-let score
-let score1
-let score2
+const init = function () {
+  // diceEl.classList.add('hidden')
+  diceEl.style.display = 'none'
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+}
 
-//Inicializar la aplicación
-initApp()
+init()
 
-btnRoll.addEventListener('click', ()=> {
+console.log(btnNew, btnRoll, btnHold)
+
+btnRoll.addEventListener('click', () => {
   //sacas un número del 1 al 6
-  const dice = Math.trunc (Math.random() * 6) + 1
-  
+  const dice = Math.trunc(Math.random() * 6) + 1
 
   //mostrar el dado
-  diceEl.src = 'dice-${dice}.png'
+  diceEl.style.display = 'block'
+  diceEl.src = `dice-${dice}.png`
 
-  if(dice === 1){
+  if (dice === 1) {
     //pierdes turno
-  } else{
+  } else {
     //sumar el dado al current score
   }
-
 })
 
 btnNew.addEventListener('click', () => {
@@ -45,16 +53,5 @@ btnHold.addEventListener('click', () => {
 })
 
 //TODO:
-//llamar a función init que haga lo siguiente:
-// el dado por defecto que no se vea
-// score1 y score2 a 0
-// currentScore1 y currentScore2 a 0
-
-//TODO:
 //cuando se hace click en el botón roll:
 //el dado se pone visible
-
-function initApp() {
-  //Inicializamos score
-  score = INIT_SCORE
-  scoreField.textContent = score}
